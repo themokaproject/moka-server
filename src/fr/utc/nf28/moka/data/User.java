4d0d66c44e03ff8def1182a513b7054ac9a33830
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-02 14:43:52 +0200

    add User class

---
+++ src/fr/utc/nf28/moka/data/User.java
@@ -0,0 +1,39 @@
+package fr.utc.nf28.moka.data;
+
+import java.awt.*;
+
+public class User {
+    private String mLastName;
+    private String mFirstName;
+    private Color mColor;
+
+    public User(String lastName, String firstName, Color color) {
+        mLastName = lastName;
+        mFirstName = firstName;
+        mColor = color;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String lastName) {
+        mLastName = lastName;
+    }
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+
+    public Color getColor() {
+        return mColor;
+    }
+
+    public void setColor(Color color) {
+        mColor = color;
+    }
+}
