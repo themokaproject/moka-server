9574451e5c859181a3b67ba434301c8e4054140e
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-02 15:17:19 +0200

    add last modifier to moka item

--- src/fr/utc/nf28/moka/data/MokaItem.java
+++ src/fr/utc/nf28/moka/data/MokaItem.java
@@ -1,11 +1,31 @@
 package fr.utc.nf28.moka.data;
 
+import java.awt.*;
+
 public abstract class MokaItem {
     private int mX;
     private int mY;
+    private User mLastModifier;
 
     public MokaItem(int x, int y) {
         mX = x;
         mY = y;
+    }
+
+    public void move(int x, int y) {
+        mX = x;
+        mY = y;
+    }
+
+    public Point getPosition() {
+        return new Point(mX,mY);
+    }
+
+    public User getLastModifier() {
+        return mLastModifier;
+    }
+
+    public void setLastModifier(User lastModifier) {
+        this.mLastModifier = lastModifier;
     }
 }
