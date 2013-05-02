44dce2a2d274b24c8ff12ba0a7a8baa9ac49fbbc
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-02 14:47:20 +0200

    add generic Item class

---
+++ src/fr/utc/nf28/moka/data/Item.java
@@ -0,0 +1,11 @@
+package fr.utc.nf28.moka.data;
+
+public abstract class Item {
+    private int mX;
+    private int mY;
+
+    public Item(int x, int y) {
+        mX = x;
+        mY = y;
+    }
+}
