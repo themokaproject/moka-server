5362937576f24cb0449856f54c6899f08d5a1b36
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-03 15:30:54 +0200

    launch a new ConnectionAgent

--- src/fr/utc/nf28/moka/Launcher.java
+++ src/fr/utc/nf28/moka/Launcher.java
@@ -1,7 +1,21 @@
 package fr.utc.nf28.moka;
 
+import fr.utc.nf28.moka.data.MokaEnvironment;
+import jade.core.ProfileException;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.StaleProxyException;
+
 public class Launcher {
     public static void main(String[] args) {
-
+        final Runtime runtime = Runtime.instance();
+        try {
+            final AgentContainer container = runtime.createMainContainer(new ProfileImpl("profile.txt"));
+            MokaEnvironment environment = new MokaEnvironment();
+            container.createNewAgent("ConnectionAgent", "fr.utc.nf28.moka.agents.ConnectionAgent", new Object[]{environment}).start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
