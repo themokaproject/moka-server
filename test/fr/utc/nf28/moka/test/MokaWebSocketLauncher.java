fd3c41689795a9c3547255dc43660315eb1ed05d
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-05-04 22:30:50 +0200

    Delete an auto-generated comment

--- test/fr/utc/nf28/moka/test/MokaWebSocketLauncher.java
+++ test/fr/utc/nf28/moka/test/MokaWebSocketLauncher.java
@@ -5,11 +5,6 @@ import org.java_websocket.WebSocketImpl;
 
 import java.net.UnknownHostException;
 
-/**
- * User: Vbarthel
- * Date: 04/05/13
- * Time: 22:25
- */
 public class MokaWebSocketLauncher {
 
 	public static void main(String[] args) throws UnknownHostException {
