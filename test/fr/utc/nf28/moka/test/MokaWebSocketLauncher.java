0348eebcd7c365bd0b8a45af90d150bcc16952e5
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-05-04 22:29:50 +0200

    Add a launcher to test MokaWebSocketServer

--- test/fr/utc/nf28/moka/test/MokaWebSocketLauncher.java
+++ test/fr/utc/nf28/moka/test/MokaWebSocketLauncher.java
@@ -1,9 +1,24 @@
 package fr.utc.nf28.moka.test;
 
+import fr.utc.nf28.moka.websocket.MokaWebSocketServer;
+import org.java_websocket.WebSocketImpl;
+
+import java.net.UnknownHostException;
+
 /**
  * User: Vbarthel
  * Date: 04/05/13
  * Time: 22:25
  */
 public class MokaWebSocketLauncher {
+
+	public static void main(String[] args) throws UnknownHostException {
+		WebSocketImpl.DEBUG = true;
+		int port = 8887;
+
+		MokaWebSocketServer test = new MokaWebSocketServer(port);
+		test.start();
+
+		System.out.println( "Server started on port: " + test.getPort() );
+	}
 }
