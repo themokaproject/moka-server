870248cd21b2bf6f0a0969d63087f1e56c98802a
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-06 16:35:32 +0200

    rename edition to itemedition

--- src/fr/utc/nf28/moka/agents/itemedition/ItemEditionAgent.java
+++ src/fr/utc/nf28/moka/agents/itemedition/ItemEditionAgent.java
@@ -1,4 +1,4 @@
-package fr.utc.nf28.moka.agents.edition;
+package fr.utc.nf28.moka.agents.itemedition;
 
 import fr.utc.nf28.moka.agents.MokaAgent;
 
@@ -6,10 +6,10 @@ import fr.utc.nf28.moka.agents.MokaAgent;
  * An agent that handles item editions
  * Send a REQUEST that contains your new item's JSON to modify it
  */
-public class EditionAgent extends MokaAgent {
+public class ItemEditionAgent extends MokaAgent {
 
     public void setup() {
         super.setup();
-        addBehaviour(new EdtionHandlingBehaviour());
+        addBehaviour(new ItemEdtionHandlingBehaviour());
     }
 }
--- src/fr/utc/nf28/moka/agents/itemedition/ItemEdtionHandlingBehaviour.java
+++ src/fr/utc/nf28/moka/agents/itemedition/ItemEdtionHandlingBehaviour.java
@@ -1,10 +1,7 @@
-package fr.utc.nf28.moka.agents.edition;
+package fr.utc.nf28.moka.agents.itemedition;
 
 import fr.utc.nf28.moka.agents.MokaAgent;
-import fr.utc.nf28.moka.agents.connection.ConnectionAgent;
-import fr.utc.nf28.moka.environment.MokaEnvironment;
 import fr.utc.nf28.moka.environment.items.MokaItem;
-import fr.utc.nf28.moka.environment.users.User;
 import fr.utc.nf28.moka.util.JSONParserUtils;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
@@ -13,9 +10,9 @@ import jade.lang.acl.MessageTemplate;
 import java.io.IOException;
 
 /**
- * A behaviour that handles item edition.
+ * A behaviour that handles item itemedition.
  */
-public class EdtionHandlingBehaviour extends CyclicBehaviour {
+public class ItemEdtionHandlingBehaviour extends CyclicBehaviour {
     @Override
     public void action() {
         final ACLMessage message = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
