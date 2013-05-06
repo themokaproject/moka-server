5de3d9dd4219f52b7cd56399530128284b63e281
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-05-06 16:20:52 +0200

    add comment

--- src/fr/utc/nf28/moka/agents/edition/EditionAgent.java
+++ src/fr/utc/nf28/moka/agents/edition/EditionAgent.java
@@ -2,6 +2,10 @@ package fr.utc.nf28.moka.agents.edition;
 
 import fr.utc.nf28.moka.agents.MokaAgent;
 
+/**
+ * An agent that handles item editions
+ * Send a REQUEST that contains your new item's JSON to modify it
+ */
 public class EditionAgent extends MokaAgent {
 
     public void setup() {
