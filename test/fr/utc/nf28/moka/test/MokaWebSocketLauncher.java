b6f708c61822e1dac9ac4fb85eda48ff95fb254e
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-05-12 18:48:36 +0200

    Add some test functions

--- test/fr/utc/nf28/moka/test/MokaWebSocketLauncher.java
+++ test/fr/utc/nf28/moka/test/MokaWebSocketLauncher.java
@@ -1,9 +1,12 @@
 package fr.utc.nf28.moka.test;
 
+import fr.utc.nf28.moka.environment.users.User;
 import fr.utc.nf28.moka.websocket.MokaWebSocketServer;
 import org.java_websocket.WebSocketImpl;
 
+import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class MokaWebSocketLauncher {
 
@@ -15,5 +18,29 @@ public class MokaWebSocketLauncher {
 		test.start();
 
 		System.out.println( "Server started on port: " + test.getPort() );
+
+		try{
+			readAction(test);
+		} catch (IOException e){
+			e.printStackTrace();;
+		}
+
 	}
+	public static void readAction(MokaWebSocketServer server) throws IOException {
+		String action;
+		Scanner scanIn = new Scanner(System.in);
+		while(true){
+			System.out.println("action :");
+			action = scanIn.nextLine();
+			System.out.println(action);
+
+			if(action.equals("1")){
+				User vb = new User();
+				vb.setFirstName("Vincent");
+				vb.setIp("127.0.0.1");
+				server.addUser(vb);
+			}
+		}
+	}
+
 }
