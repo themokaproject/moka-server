67f2abbe86342c218c2e035195b34bc4a30ee74e
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-05-14 15:10:34 +0200

    Factorize MokaWebSocketServer

--- src/fr/utc/nf28/moka/websocket/MokaWebsocketServer.java
+++ src/fr/utc/nf28/moka/websocket/MokaWebsocketServer.java
@@ -3,10 +3,7 @@ package fr.utc.nf28.moka.websocket;
 import fr.utc.nf28.moka.environment.items.MokaItem;
 import fr.utc.nf28.moka.environment.users.User;
 import fr.utc.nf28.moka.util.JSONParserUtils;
-import fr.utc.nf28.moka.websocket.request.AddItemWebSocketRequest;
-import fr.utc.nf28.moka.websocket.request.AddUserWebSocketRequest;
-import fr.utc.nf28.moka.websocket.request.RemoveItemWebSocketRequest;
-import fr.utc.nf28.moka.websocket.request.RemoveUserWebSocketRequest;
+import fr.utc.nf28.moka.websocket.request.*;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
@@ -14,7 +11,6 @@ import org.java_websocket.server.WebSocketServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 public class MokaWebSocketServer extends WebSocketServer {
 
@@ -52,28 +48,32 @@ public class MokaWebSocketServer extends WebSocketServer {
 
 	public void addUser(User user) throws IOException {
 		AddUserWebSocketRequest request = new AddUserWebSocketRequest(user);
-		sendAll(JSONParserUtils.serializeWebSocketRequest(request));
+		sendAll(request);
 	}
 
 	public void removeUser(User user) throws IOException {
 		RemoveUserWebSocketRequest request = new RemoveUserWebSocketRequest(user);
-	    sendAll(JSONParserUtils.serializeWebSocketRequest(request));
+		sendAll(request);
 	}
 
 	public void addItem(MokaItem item) throws IOException {
 		AddItemWebSocketRequest request = new AddItemWebSocketRequest(item);
-	 	sendAll(JSONParserUtils.serializeWebSocketRequest(request));
+		sendAll(request);
 	}
 
 	public void removeItem(MokaItem item) throws IOException {
 		RemoveItemWebSocketRequest request = new RemoveItemWebSocketRequest(item);
-		sendAll(JSONParserUtils.serializeWebSocketRequest(request));
+		sendAll(request);
 	}
 
 	public void sendAll(String message){
 		for(WebSocket connection : this.connections()){
 			connection.send(message);
 		}
+	}
+
+	public void sendAll(WebSocketRequest request) throws IOException {
+		sendAll(JSONParserUtils.serializeWebSocketRequest(request));
 	}
 
 
