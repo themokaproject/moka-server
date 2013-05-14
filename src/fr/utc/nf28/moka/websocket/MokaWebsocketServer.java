0c6c738d1a80a8a76681e519866f0e13ab08b817
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-05-14 14:34:41 +0200

    Indent

--- src/fr/utc/nf28/moka/websocket/MokaWebsocketServer.java
+++ src/fr/utc/nf28/moka/websocket/MokaWebsocketServer.java
@@ -14,47 +14,46 @@ import java.util.Scanner;
 
 public class MokaWebSocketServer extends WebSocketServer {
 
-    public MokaWebSocketServer(int port) throws UnknownHostException {
-        super(new InetSocketAddress(port));
-    }
+	public MokaWebSocketServer(int port) throws UnknownHostException {
+		super(new InetSocketAddress(port));
+	}
 
-    public MokaWebSocketServer(InetSocketAddress add) {
-        super(add);
-    }
+	public MokaWebSocketServer(InetSocketAddress add) {
+		super(add);
+	}
 
-    @Override
-    public void onOpen(WebSocket connection, ClientHandshake clientHandshake) {
-        //TODO onOpen
-        System.out.println(connection.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
-    }
+	@Override
+	public void onOpen(WebSocket connection, ClientHandshake clientHandshake) {
+		//TODO onOpen
+		System.out.println(connection.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
+	}
 
-    @Override
-    public void onClose(WebSocket connection, int i, String s, boolean b) {
-        //TODO onClose
-        System.out.println(connection + " has left the room!");
-    }
+	@Override
+	public void onClose(WebSocket connection, int i, String s, boolean b) {
+		//TODO onClose
+		System.out.println(connection + " has left the room!");
+	}
 
-    @Override
-    public void onMessage(WebSocket connection, String message) {
-        //TODO onMessage
-        System.out.println(connection + ": " + message);
-    }
+	@Override
+	public void onMessage(WebSocket connection, String message) {
+		//TODO onMessage
+		System.out.println(connection + ": " + message);
+	}
 
-    @Override
-    public void onError(WebSocket webSocket, Exception ex) {
-        //TODO onError
-        ex.printStackTrace();
-    }
+	@Override
+	public void onError(WebSocket webSocket, Exception ex) {
+		//TODO onError
+		ex.printStackTrace();
+	}
 
-	private void addUser(User user) throws IOException{
+	public void addUser(User user) throws IOException{
 		AddUserWebSocketRequest request = new AddUserWebSocketRequest(user);
 		sendAll(JSONParserUtils.serializeWebSocketRequest(request));
 	}
 
-
 	public void sendAll(String message){
 		for(WebSocket connection : this.connections()){
-			  connection.send(message);
+			connection.send(message);
 		}
 	}
 
