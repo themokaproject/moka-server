76e7acd89e9c48e6031fcaef69662f8b88cbea83
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-19 15:32:58 +0200

    Create JadeUtils to centralize skill's names and types

---
+++ src/fr/utc/nf28/moka/util/JadeUtils.java
@@ -0,0 +1,11 @@
+package fr.utc.nf28.moka.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hi-Bro
+ * Date: 19/05/13
+ * Time: 15:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class JadeUtils {
+}
