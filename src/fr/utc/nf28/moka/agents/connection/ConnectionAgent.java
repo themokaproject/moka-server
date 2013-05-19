2e891b64ababcedee3ae9d06328edd09945e7c1a
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-19 15:39:48 +0200

    Register Connection Agent to the DF

--- src/fr/utc/nf28/moka/agents/connection/ConnectionAgent.java
+++ src/fr/utc/nf28/moka/agents/connection/ConnectionAgent.java
@@ -1,6 +1,7 @@
 package fr.utc.nf28.moka.agents.connection;
 
 import fr.utc.nf28.moka.agents.MokaAgent;
+import fr.utc.nf28.moka.util.JadeUtils;
 
 /**
  * An agent that handles connections and disconnections of clients to Moka
@@ -10,6 +11,7 @@ public class ConnectionAgent extends MokaAgent {
 
     public void setup() {
         super.setup();
+		registerSkill(JadeUtils.JADE_SKILL_NAME_CONNECTION);
         addBehaviour(new ConnectionHandlingBehaviour());
     }
 }
