8dcab92634a568230625799577262bddcf51df62
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-20 12:25:03 +0200

    Register CreationAgent with skill name

--- src/fr/utc/nf28/moka/agents/itemcreation/ItemCreationAgent.java
+++ src/fr/utc/nf28/moka/agents/itemcreation/ItemCreationAgent.java
@@ -1,6 +1,7 @@
 package fr.utc.nf28.moka.agents.itemcreation;
 
 import fr.utc.nf28.moka.agents.MokaAgent;
+import fr.utc.nf28.moka.util.JadeUtils;
 
 /**
  * An agent that creates items. Send a REQUEST with a creation JSON to this agent to ad an item
@@ -9,6 +10,7 @@ public class ItemCreationAgent extends MokaAgent {
 
     public void setup() {
         super.setup();
+		registerSkill(JadeUtils.JADE_SKILL_NAME_CREATION);
         addBehaviour(new ItemCreationBehaviour());
     }
 }
