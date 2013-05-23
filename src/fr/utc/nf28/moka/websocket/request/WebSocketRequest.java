5124df816cf1b30d2179b6db3d121c55a07ba33c
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-05-23 12:21:11 +0200

    WebSocketRequest:content is now a HashMap<String,String>

--- src/fr/utc/nf28/moka/websocket/request/WebSocketRequest.java
+++ src/fr/utc/nf28/moka/websocket/request/WebSocketRequest.java
@@ -1,19 +1,25 @@
 package fr.utc.nf28.moka.websocket.request;
 
+import java.util.HashMap;
+
 /**
  * This class is a base for all the web socket request
  * between the web socket server and the clients.
  */
 public class WebSocketRequest {
 	public String type;
-	public Object content ;
+	public HashMap<String, String> content ;
 
 	public WebSocketRequest(String type) {
 		this.type = type;
 	}
 
-	public WebSocketRequest(String type, Object content) {
+	public WebSocketRequest(String type, HashMap<String, String> content) {
 		this.type = type;
 		this.content = content;
+	}
+
+	public void put(String key, String value) {
+		this.content.put(key, value);
 	}
 }
