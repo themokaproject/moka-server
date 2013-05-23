760aeb0eb937cdd94c56a82384e4ea7571ec98cc
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-23 10:41:33 +0200

    Propagate umlCreation to WebsocketAgent

--- src/fr/utc/nf28/moka/agents/itemcreation/ItemCreationAgent.java
+++ src/fr/utc/nf28/moka/agents/itemcreation/ItemCreationAgent.java
@@ -1,16 +1,40 @@
 package fr.utc.nf28.moka.agents.itemcreation;
 
+import fr.utc.nf28.moka.agents.A2ATransaction;
 import fr.utc.nf28.moka.agents.MokaAgent;
+import fr.utc.nf28.moka.environment.items.UmlClass;
+import fr.utc.nf28.moka.util.JSONParserUtils;
 import fr.utc.nf28.moka.util.JadeUtils;
+
+import java.io.IOException;
 
 /**
  * An agent that creates items. Send a REQUEST with a creation JSON to this agent to ad an item
  */
 public class ItemCreationAgent extends MokaAgent {
 
-    public void setup() {
-        super.setup();
+	public void setup() {
+		super.setup();
 		registerSkill(JadeUtils.JADE_SKILL_NAME_CREATION);
-        addBehaviour(new ItemCreationBehaviour());
-    }
+		addBehaviour(new ItemCreationBehaviour());
+	}
+
+	/**
+	 * create new item
+	 *
+	 * @param type typeof this new item
+	 * @throws IOException
+	 */
+	public void create(String type) throws IOException {
+		if (type.equals("umlClass")) {
+			final UmlClass uml = new UmlClass("MyClass", 200, 350, "UmlClass");
+			uml.setId(getEnvironment().generateNewId());
+			getEnvironment().addItem(uml);
+			//TODO communicate the item's id to the app
+			final A2ATransaction transaction = new A2ATransaction("addItem", JSONParserUtils.serializeItem(uml));
+			sendMessage(getAgentsWithSkill(JadeUtils.JADE_SKILL_NAME_WEBSOCKET_SERVER),
+					JSONParserUtils.serializeA2ATransaction(transaction),
+					jade.lang.acl.ACLMessage.PROPAGATE);
+		}
+	}
 }
--- src/fr/utc/nf28/moka/agents/itemcreation/ItemCreationBehaviour.java
+++ src/fr/utc/nf28/moka/agents/itemcreation/ItemCreationBehaviour.java
@@ -2,8 +2,6 @@ package fr.utc.nf28.moka.agents.itemcreation;
 
 import fr.utc.nf28.moka.agents.A2ATransaction;
 import fr.utc.nf28.moka.agents.MokaAgent;
-import fr.utc.nf28.moka.environment.MokaEnvironment;
-import fr.utc.nf28.moka.environment.items.MokaItem;
 import fr.utc.nf28.moka.util.JSONParserUtils;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
@@ -15,40 +13,31 @@ import java.io.IOException;
  * A behaviour that handles item creation.
  */
 public class ItemCreationBehaviour extends CyclicBehaviour {
-    @Override
-    public void action() {
-        final ACLMessage message = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
-        if (message != null) {
-            final String requestString = message.getContent();
-            try {
+	@Override
+	public void action() {
+		final ACLMessage message = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
+		if (message != null) {
+			final String requestString = message.getContent();
+			try {
 				final A2ATransaction request = JSONParserUtils.deserializeA2ATransaction(requestString);
 				final String type = request.getType();
-                if(type.equals("creation")) {
-                    create(type.getRequest());
-                } else if (type.equals("destruction")) {
-                    destroy(type.getRequest());
-                } else {
-                    throw new IOException();
-                }
-            } catch (IOException e) {
-                System.out.println("Item creation/destruction request syntax is wrong");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+				if (type.equals("creation")) {
+					((ItemCreationAgent)myAgent).create((String)request.getContent());
+				} else if (type.equals("destruction")) {
+					//TODO implement destruction
+				} else {
+					throw new IOException();
+				}
+			} catch (IOException e) {
+				System.out.println("Item creation/destruction request syntax is wrong");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    private void destroy(String idString) {
-        ((MokaAgent)myAgent).getEnvironment().removeItem(Integer.valueOf(idString));
-    }
+	private void destroy(String idString) {
+		((MokaAgent) myAgent).getEnvironment().removeItem(Integer.valueOf(idString));
+	}
 
-    private void create(String itemString) throws IOException {
-        MokaItem item = JSONParserUtils.deserializeItem(itemString);
-        MokaEnvironment environment = ((MokaAgent)myAgent).getEnvironment();
-        int newId = environment.generateNewId();
-        // TODO communicate the item's id to the app
-        item.setId(newId);
-        environment.addItem(item);
-    }
-    
 }
