5b13fdc6df64c3650bd8a95d898b084c0adaafba
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-26 10:00:29 +0200

    Add methode to send message to all Android device

--- src/fr/utc/nf28/moka/agents/MokaAgent.java
+++ src/fr/utc/nf28/moka/agents/MokaAgent.java
@@ -1,24 +1,36 @@
 package fr.utc.nf28.moka.agents;
 
 import fr.utc.nf28.moka.environment.MokaEnvironment;
-import jade.core.Agent;
+import fr.utc.nf28.moka.util.JadeUtils;
 
 /**
  * A generic Agent for Moka
  * Has a reference to its MokaEnvironment
  */
 public class MokaAgent extends BaseAgent {
-    protected MokaEnvironment mEnvironment;
+	protected MokaEnvironment mEnvironment;
 
-    public void setup() {
+	public void setup() {
 		super.setup();
-        final Object[] args = getArguments();
-        mEnvironment = (MokaEnvironment)args[0];
-    }
+		final Object[] args = getArguments();
+		mEnvironment = (MokaEnvironment) args[0];
+	}
 
-    public MokaEnvironment getEnvironment() {
-        return mEnvironment;
-    }
+	public MokaEnvironment getEnvironment() {
+		return mEnvironment;
+	}
+
+	/**
+	 * send message to all android devices
+	 *
+	 * @param performatif message's performatif
+	 * @param content     message's content
+	 */
+	protected void sendMessageToAndroidDevice(final int performatif, final String content) {
+		sendMessage(getAgentsWithSkill(JadeUtils.JADE_SKILL_NAME_ANDROID),
+				content,
+				performatif);
+	}
 
 
 }
