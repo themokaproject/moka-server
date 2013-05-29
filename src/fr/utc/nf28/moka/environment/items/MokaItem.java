ca2f6422c3965230b55fe1ccd8604656e539cf22
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-05-29 16:14:43 +0200

    Add width and height to MokaItem

--- src/fr/utc/nf28/moka/environment/items/MokaItem.java
+++ src/fr/utc/nf28/moka/environment/items/MokaItem.java
@@ -1,92 +1,108 @@
 package fr.utc.nf28.moka.environment.items;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import fr.utc.nf28.moka.environment.users.User;
 
 /**
  * A generic visual item.
- *
  */
 public abstract class MokaItem {
-    private static int sIdIndex = 0;
-    private int mId;
-    private String mTitle;
-    private int mX;
-    private int mY;
-    private transient User mLocker;
+	private static int sIdIndex = 0;
+	private int mId;
+	private String mTitle;
+	private int mX;
+	private int mY;
+	private int mHeight;
+	private int mWidth;
+	private transient User mLocker;
 
-    public MokaItem() {
-        mId = sIdIndex++;
-    }
+	public MokaItem() {
+		mId = sIdIndex++;
+	}
 
-    public MokaItem(String title, int x, int y) {
-        this();
-        mTitle = title;
-        mX = x;
-        mY = y;
-    }
+	public MokaItem(String title, int x, int y) {
+		this();
+		mTitle = title;
+		mX = x;
+		mY = y;
+	}
 
-    public int getId() {
-        return mId;
-    }
+	public int getId() {
+		return mId;
+	}
 
-    public void setId(int mId) {
-        this.mId = mId;
-    }
+	public void setId(int mId) {
+		this.mId = mId;
+	}
 
-    public String getTitle() {
-        return mTitle;
-    }
+	public String getTitle() {
+		return mTitle;
+	}
 
-    public void setTitle(String title) {
-        this.mTitle = title;
-    }
+	public void setTitle(String title) {
+		this.mTitle = title;
+	}
 
-    public int getY() {
-        return mY;
-    }
+	public int getY() {
+		return mY;
+	}
 
-    public void setY(int mY) {
-        this.mY = mY;
-    }
+	public void setY(int mY) {
+		this.mY = mY;
+	}
 
-    public int getX() {
-        return mX;
-    }
+	public int getX() {
+		return mX;
+	}
 
-    public void setX(int x) {
-        this.mX = x;
-    }
+	public void setX(int x) {
+		this.mX = x;
+	}
 
-    public void move(int x, int y) {
-        mX = x;
-        mY = y;
-    }
+	public int getHeight() {
+		return mHeight;
+	}
 
-    @JsonIgnore
-    public boolean isLocked() {
-        return mLocker == null;
-    }
+	public void setHeight(int height) {
+		mHeight = height;
+	}
 
-    @JsonIgnore
-    public User getLocker() {
-        return mLocker;
-    }
+	public int getWidth() {
+		return mWidth;
+	}
 
-    public void lock(User locker) {
-        if(mLocker != null) {
-            System.out.println("Item " + toString() + "already locked");
-            return;
-        }
-        mLocker = locker;
-    }
+	public void setWidth(int width) {
+		mWidth = width;
+	}
 
-    public void unlock() {
-        mLocker = null;
-    }
+	public void move(int x, int y) {
+		mX = x;
+		mY = y;
+	}
 
-    public String toString() {
-        return "title:" + mTitle + " id:" + mId + " x:" + mX + " y:" + mY;
-    }
+	@JsonIgnore
+	public boolean isLocked() {
+		return mLocker == null;
+	}
+
+	@JsonIgnore
+	public User getLocker() {
+		return mLocker;
+	}
+
+	public void lock(User locker) {
+		if (mLocker != null) {
+			System.out.println("Item " + toString() + "already locked");
+			return;
+		}
+		mLocker = locker;
+	}
+
+	public void unlock() {
+		mLocker = null;
+	}
+
+	public String toString() {
+		return "title:" + mTitle + " id:" + mId + " x:" + mX + " y:" + mY;
+	}
 }
