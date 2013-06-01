14b4b60eb3947339107ca7ea80d8b959d314fcc5
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-06-01 17:15:15 +0200

    Send callback to Android (remove 3 TODO)

--- src/fr/utc/nf28/moka/agents/itemedition/ItemLockingAgent.java
+++ src/fr/utc/nf28/moka/agents/itemedition/ItemLockingAgent.java
@@ -1,10 +1,15 @@
 package fr.utc.nf28.moka.agents.itemedition;
 
+import fr.utc.nf28.moka.agents.A2ATransaction;
 import fr.utc.nf28.moka.agents.MokaAgent;
 import fr.utc.nf28.moka.environment.MokaEnvironment;
 import fr.utc.nf28.moka.environment.users.User;
+import fr.utc.nf28.moka.util.JSONParserUtils;
 import fr.utc.nf28.moka.util.JadeUtils;
 import jade.core.AID;
+import jade.lang.acl.ACLMessage;
+
+import java.io.IOException;
 
 /**
  * An agent that handles mutual exclusivity when a user edits an item
@@ -30,17 +35,22 @@ public class ItemLockingAgent extends MokaAgent {
 	/**
 	 * use to lock item
 	 *
-	 * @param itemId item id
+	 * @param itemId   item id
+	 * @param userAID  AndroidAgent requester AID
+	 * @param response response which will be send to requester
 	 */
-	public void lockItem(int itemId, AID userAID) {
+	public void lockItem(int itemId, AID userAID, ACLMessage response) throws IOException {
 		final MokaEnvironment environment = getEnvironment();
 		final User locker = environment.lockItem(itemId, userAID.toString());
+		final A2ATransaction transaction;
 		if (locker == null) {
-			//TODO send error
+			transaction = new A2ATransaction(JadeUtils.TRANSACTION_TYPE_LOCK_ITEM_ERROR, "");
 		} else if (locker.getAID().equals(userAID)) {
-			//TODO send success
+			transaction = new A2ATransaction(JadeUtils.TRANSACTION_TYPE_LOCK_ITEM_SUCCESS, "");
 		} else {
-			//TODO send already locked
+			transaction = new A2ATransaction(JadeUtils.TRANSACTION_TYPE_LOCK_ITEM_ALREADY, locker.makePseudo());
 		}
+		response.setContent(JSONParserUtils.serializeA2ATransaction(transaction));
+		send(response);
 	}
 }
--- src/fr/utc/nf28/moka/agents/itemedition/ItemLockingBehaviour.java
+++ src/fr/utc/nf28/moka/agents/itemedition/ItemLockingBehaviour.java
@@ -21,10 +21,10 @@ public class ItemLockingBehaviour extends CyclicBehaviour {
 				if (JadeUtils.TRANSACTION_TYPE_UNLOCK_ITEM.equals(type)) {
 					((ItemLockingAgent) myAgent).unlockItem((Integer) request.getContent());
 				} else if (JadeUtils.TRANSACTION_TYPE_LOCK_ITEM.equals(type)) {
-					((ItemLockingAgent) myAgent).lockItem((Integer) request.getContent(), message.getSender());
+					((ItemLockingAgent) myAgent).lockItem((Integer) request.getContent(), message.getSender(),message.createReply());
 				}
 			} catch (IOException e) {
-				e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+				e.printStackTrace();
 			}
 		}
 	}
