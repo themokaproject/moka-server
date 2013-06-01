d202c2de91085452280645c32f810c3a0f321098
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-06-01 15:54:58 +0200

    Implement ItemLockingBehavior skeleton

--- src/fr/utc/nf28/moka/agents/itemedition/ItemLockingBehaviour.java
+++ src/fr/utc/nf28/moka/agents/itemedition/ItemLockingBehaviour.java
@@ -1,11 +1,8 @@
 package fr.utc.nf28.moka.agents.itemedition;
 
-import fr.utc.nf28.moka.agents.MokaAgent;
-import fr.utc.nf28.moka.environment.MokaEnvironment;
-import fr.utc.nf28.moka.environment.items.LockingRequest;
-import fr.utc.nf28.moka.environment.items.MokaItem;
-import fr.utc.nf28.moka.environment.users.User;
+import fr.utc.nf28.moka.agents.A2ATransaction;
 import fr.utc.nf28.moka.util.JSONParserUtils;
+import fr.utc.nf28.moka.util.JadeUtils;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
@@ -13,21 +10,24 @@ import jade.lang.acl.MessageTemplate;
 import java.io.IOException;
 
 public class ItemLockingBehaviour extends CyclicBehaviour {
-    @Override
-    public void action() {
-        final ACLMessage message = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
-        if (message != null) {
-            final String lockingRequestJson = message.getContent();
-            try {
-                LockingRequest lockingRequest = JSONParserUtils.deserializeLockingRequest(lockingRequestJson);
-                MokaEnvironment environment = ((MokaAgent) myAgent).getEnvironment();
-                // TODO reimplement locking
-                System.out.println("no item with id " + lockingRequest.itemId);
-            } catch (IOException e) {
-                System.out.println("Locking request syntax is wrong");
-            }
-
-
-        }
-    }
+	@Override
+	public void action() {
+		final ACLMessage message = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
+		if (message != null) {
+			final String requestString = message.getContent();
+			try {
+				final A2ATransaction request = JSONParserUtils.deserializeA2ATransaction(requestString);
+				final String type = request.getType();
+				if (JadeUtils.TRANSACTION_TYPE_UNLOCK_ITEM.equals(type)) {
+					//TODO implement unlocking
+					System.out.println("unlocking requested !");
+				} else if (JadeUtils.TRANSACTION_TYPE_LOCK_ITEM.equals(type)) {
+					//TODO implement locking
+					System.out.println("locking requested !");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+			}
+		}
+	}
 }
