e8f3c369449af818f6beeb7990793971fddd2761
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-06-01 15:43:10 +0200

    Register Locking agent to the DF

--- src/fr/utc/nf28/moka/agents/itemedition/ItemLockingAgent.java
+++ src/fr/utc/nf28/moka/agents/itemedition/ItemLockingAgent.java
@@ -1,6 +1,7 @@
 package fr.utc.nf28.moka.agents.itemedition;
 
 import fr.utc.nf28.moka.agents.MokaAgent;
+import fr.utc.nf28.moka.util.JadeUtils;
 
 /**
  * An agent that handles mutual exclusivity when a user edits an item
@@ -9,6 +10,7 @@ public class ItemLockingAgent extends MokaAgent {
 
     public void setup() {
         super.setup();
+		registerSkill(JadeUtils.JADE_SKILL_NAME_LOCKING);
         addBehaviour(new ItemLockingBehaviour());
     }
 }
