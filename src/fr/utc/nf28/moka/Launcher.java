6240a99a0dfd57b4be2b13e9a5c23978877405ee
Thomas <thomas.keunebroek@gmail.com>
2013-06-04 14:58:24 +0200

    Fix indentation

--- src/fr/utc/nf28/moka/Launcher.java
+++ src/fr/utc/nf28/moka/Launcher.java
@@ -11,13 +11,18 @@ public class Launcher {
 		try {
 			final AgentContainer container = runtime.createMainContainer(new ProfileImpl("profile.txt"));
 			final MokaEnvironment environment = MokaEnvironment.getInstance();
-			container.createNewAgent("ConnectionAgent", "fr.utc.nf28.moka.agents.connection.ConnectionAgent", new Object[]{environment}).start();
-			container.createNewAgent("ItemCreationAgent", "fr.utc.nf28.moka.agents.itemcreation.ItemCreationAgent", new Object[]{environment}).start();
-			container.createNewAgent("ItemEditionAgent", "fr.utc.nf28.moka.agents.itemedition.ItemEditionAgent", new Object[]{environment}).start();
-			container.createNewAgent("ItemLockingAgent", "fr.utc.nf28.moka.agents.itemedition.ItemLockingAgent", new Object[]{environment}).start();
-			container.createNewAgent("WebSocketAgent", "fr.utc.nf28.moka.agents.websocket.WebSocketAgent", null).start();
-			container.createNewAgent("RestServerAgent", "fr.utc.nf28.moka.agents.rest.RestServerAgent", null).start();
-
+			container.createNewAgent("ConnectionAgent", "fr.utc.nf28.moka.agents.connection.ConnectionAgent",
+					new Object[]{environment}).start();
+			container.createNewAgent("ItemCreationAgent", "fr.utc.nf28.moka.agents.itemcreation.ItemCreationAgent",
+					new Object[]{environment}).start();
+			container.createNewAgent("ItemEditionAgent", "fr.utc.nf28.moka.agents.itemedition.ItemEditionAgent",
+					new Object[]{environment}).start();
+			container.createNewAgent("ItemLockingAgent", "fr.utc.nf28.moka.agents.itemedition.ItemLockingAgent",
+					new Object[]{environment}).start();
+			container.createNewAgent("WebSocketAgent", "fr.utc.nf28.moka.agents.websocket.WebSocketAgent",
+					null).start();
+			container.createNewAgent("RestServerAgent", "fr.utc.nf28.moka.agents.rest.RestServerAgent",
+					null).start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
