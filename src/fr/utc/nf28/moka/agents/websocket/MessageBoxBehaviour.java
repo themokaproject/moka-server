677a6f7f5a2cb63b1a8bb56dbb7802e47196a6a3
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-06-05 21:00:56 +0200

    "Optimize" a little

--- src/fr/utc/nf28/moka/agents/websocket/MessageBoxBehaviour.java
+++ src/fr/utc/nf28/moka/agents/websocket/MessageBoxBehaviour.java
@@ -26,32 +26,33 @@ public class MessageBoxBehaviour extends CyclicBehaviour {
 				final A2ATransaction transaction = JSONParserUtils.deserializeA2ATransaction(message.getContent());
 				WebSocketRequest request = null;
 				final String messageToSend;
-				if (JadeUtils.TRANSACTION_TYPE_CONNECTION.equals(transaction.getType())) {
+				final String transactionType = transaction.getType();
+				if (JadeUtils.TRANSACTION_TYPE_CONNECTION.equals(transactionType)) {
 					final User user = (User) transaction.getContent();
 					request = WebSocketRequestFactory.createAddUserRequest(user.getIp(), user.makePseudo());
-				} else if (JadeUtils.TRANSACTION_TYPE_ADD_ITEM.equals(transaction.getType())) {
+				} else if (JadeUtils.TRANSACTION_TYPE_ADD_ITEM.equals(transactionType)) {
 					final MokaItem item = (MokaItem) transaction.getContent();
 					request = WebSocketRequestFactory.createAddItemRequest(item.getType(), item.getId(), item.getX(),
 							item.getY(), item.getWidth(), item.getHeight(), item.getTitle(), item.getRotateX(), item.getRotateY(), item.getRotateZ());
-				} else if (JadeUtils.TRANSACTION_TYPE_DELETE_ITEM.equals(transaction.getType())) {
+				} else if (JadeUtils.TRANSACTION_TYPE_DELETE_ITEM.equals(transactionType)) {
 					final String id = String.valueOf(transaction.getContent());
 					request = WebSocketRequestFactory.createRemoveItemRequest(id);
-				} else if (JadeUtils.TRANSACTION_TYPE_MOVE_ITEM.equals(transaction.getType())) {
+				} else if (JadeUtils.TRANSACTION_TYPE_MOVE_ITEM.equals(transactionType)) {
 					final MokaItem item = (MokaItem) transaction.getContent();
 					request = WebSocketRequestFactory.createMoveItemRequest(item.getId(), item.getX(), item.getY());
-				} else if (JadeUtils.TRANSACTION_TYPE_RESIZE_ITEM.equals(transaction.getType())) {
+				} else if (JadeUtils.TRANSACTION_TYPE_RESIZE_ITEM.equals(transactionType)) {
 					final MokaItem item = (MokaItem) transaction.getContent();
 					request = WebSocketRequestFactory.createResizeItemRequest(item.getId(), item.getWidth(), item.getHeight());
-				} else if (JadeUtils.TRANSACTION_TYPE_LOCK_ITEM_SUCCESS.equals(transaction.getType())) {
+				} else if (JadeUtils.TRANSACTION_TYPE_LOCK_ITEM_SUCCESS.equals(transactionType)) {
 					final HashMap<String, String> info = (HashMap<String, String>) transaction.getContent();
 					request = WebSocketRequestFactory.createSelectItemRequest(info.get("userId"), info.get("itemId"));
-				} else if (JadeUtils.TRANSACTION_TYPE_UNLOCK_ITEM.equals(transaction.getType())) {
+				} else if (JadeUtils.TRANSACTION_TYPE_UNLOCK_ITEM.equals(transactionType)) {
 					final int itemId = (Integer) transaction.getContent();
 					request = WebSocketRequestFactory.createUnselectItemRequest(itemId);
-				} else if (JadeUtils.TRANSACTION_TYPE_ROTATE_ITEM.equals(transaction.getType())) {
+				} else if (JadeUtils.TRANSACTION_TYPE_ROTATE_ITEM.equals(transactionType)) {
 					final MokaItem item = (MokaItem) transaction.getContent();
 					request = WebSocketRequestFactory.createRotateItemRequest(item.getId(), item.getRotateX(), item.getRotateY(), item.getRotateZ());
-				} else if(JadeUtils.TRANSACTION_TYPE_EDIT_ITEM.equals(transaction.getType())) {
+				} else if(JadeUtils.TRANSACTION_TYPE_EDIT_ITEM.equals(transactionType)) {
 					final HashMap<String, Object> infos = (HashMap<String, Object>) transaction.getContent();
 					request = WebSocketRequestFactory.createEditRequest(infos);
 				}
