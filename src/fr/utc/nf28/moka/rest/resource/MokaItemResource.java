e360423a1d56fc6226371d9c7d71b7c0041c213a
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-06-06 18:34:45 +0200

    Revert "Reverse currentItemsList on server side (need improvement)"
    This reverts commit 8101f4ad05e3af5c83ada56edd3959cb2ddaa1a9.

--- src/fr/utc/nf28/moka/rest/resource/MokaItemResource.java
+++ src/fr/utc/nf28/moka/rest/resource/MokaItemResource.java
@@ -1,21 +1,15 @@
 package fr.utc.nf28.moka.rest.resource;
 
 import fr.utc.nf28.moka.environment.MokaEnvironment;
-import fr.utc.nf28.moka.environment.items.MokaItem;
 import fr.utc.nf28.moka.util.JSONParserUtils;
 
 import javax.ws.rs.Path;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 
 @Path("/item/list")
 public class MokaItemResource extends MokaRestResource {
 	@Override
 	public String serialize() throws IOException {
-		//TODO improve way to reverse items....
-		final ArrayList<MokaItem> list = new ArrayList<MokaItem>(MokaEnvironment.getInstance().getItems().values());
-		Collections.reverse(list);
-		return JSONParserUtils.serializeToJson(list);
+		return JSONParserUtils.serializeToJson(MokaEnvironment.getInstance().getItems().values());
 	}
 }
